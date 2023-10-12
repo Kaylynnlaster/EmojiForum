fd0d27d273fa04247138c696d8f53f8d6f9243d6
Kaylynnlaster <Kayannalynnlaster@gmail.com>
2023-10-12 13:55:40 -0500

    Set up each model to include an id inside for easier reference

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,11 +1,14 @@
+const { Double } = require('mongodb');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
+    id: String,
     content: String,
     user: {
         username: String,
     },
+    //save as objectId or something similar
     threadID: String,
     createdAt: Date
 });
