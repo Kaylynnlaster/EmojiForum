49b66dba8bcef777e7e27501cc006d3d6e9ee1fa
Vinson Martin <vm84039@yahoo.com>
2023-10-18 12:32:08 -0700

    Updated Footer & Home Page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,14 +4,14 @@ import Container from "react-bootstrap/esm/Container";
 
 const CustFooter = () => {
   return (
-    <Container className="FullFooter">
-      <div className="FooterB">
+    <footer className="FullFooter">
+      <Container className="">
         <h1>The Forum for Emoji Lovers</h1>
-      </div>
-      <div className="Content">
-        <h3>Copyright</h3>
-      </div>
-    </Container>
+        <div className="Content">
+          <h3>Copyright &#169;</h3>
+        </div>
+      </Container>
+    </footer>
   );
 };
 
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -14,13 +14,14 @@ export const Home = () => {
   const [rowData, setRowData] = useState([]);
 
   const fetchData = async () => {
+    if (!isLoggedIn) {
+      navigate("/login"); // Redirect to login page if not logged in
+      return;
+    }
+
     try {
       const response = await ThreadApi.getAllThreads();
       if (!response.ok) {
-        if (response.status === 401) {
-          navigate("/login"); // Redirect to login page if not logged in
-          return;
-        }
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const threads = await response.json();
@@ -71,70 +72,72 @@ export const Home = () => {
         <Container className="new-thread">
           <Button onClick={() => navigate("/newthread")}>New Thread</Button>
         </Container>
-        {[...rowData].reverse().map((thread, index) => (
-          <div key={index}>
-            <Container className="title-container">
-              <div className="title-user">
-                User:<p className="title-content">{thread.user.username}</p>
-              </div>
-              <div className="title-header">{thread.title}</div>
-              <div className="title-date">
-                Posted On:<p className="title-content">{thread.createdAt}</p>
-              </div>
-              <div className="title-replies">
-                # of Comments:
-                <p className="title-content">{thread.commentsCount}</p>
-              </div>
-            </Container>
-            <Container className="content-container">
-              {thread.description.map((desc, descIndex) => (
-                <div className="content-content" key={descIndex}>
-                  {desc.split(",").map((code, idx) => (
-                    <p key={idx}>
-                      <Emoji unified={code.trim()} size="50" />
-                    </p>
-                  ))}
+        <div className="scroll">
+          {[...rowData].reverse().map((thread, index) => (
+            <div key={index}>
+              <Container className="title-container">
+                <div className="title-user">
+                  User:<p className="title-content">{thread.user.username}</p>
                 </div>
-              ))}
-            </Container>
-            <Container className="comments-container">
-              <div className="title-content">Comments</div>
+                <div className="title-header">{thread.title}</div>
+                <div className="title-date">
+                  Posted On:<p className="title-content">{thread.createdAt}</p>
+                </div>
+                <div className="title-replies">
+                  # of Comments:
+                  <p className="title-content">{thread.commentsCount}</p>
+                </div>
+              </Container>
+              <Container className="content-container">
+                {thread.description.map((desc, descIndex) => (
+                  <div className="content-content" key={descIndex}>
+                    {desc.split(",").map((code, idx) => (
+                      <p key={idx}>
+                        <Emoji unified={code.trim()} size="50" />
+                      </p>
+                    ))}
+                  </div>
+                ))}
+              </Container>
+              <Container className="comments-container">
+                <div className="title-content">Comments</div>
 
-              {thread.commentsCount === 0 ? (
-                <Button
-                  onClick={() =>
-                    navigate("/newcomment", {
-                      state: { selectedThreadId: thread._id },
-                    })
-                  }
-                  className="btn-link no-hover nowrap-button"
-                >
-                  New Comment
-                </Button>
-              ) : (
-                // If there are replies, show "View" button
-                <Button
-                  onClick={() => toggleThread(thread._id)}
-                  aria-controls={`collapse${thread._id}`}
-                  aria-expanded={openThreads[thread._id]}
-                  className="btn-link no-hover nowrap-button"
-                >
-                  {openThreads[thread._id] ? "Hide" : "View"}
-                </Button>
-              )}
-            </Container>
-            <Collapse in={openThreads[thread._id]}>
-              <div id={`collapse${thread._id}`}>
-                <Container className="comments-container">
-                  <AsyncComments
-                    userId={thread.user._id}
-                    threadId={thread._id}
-                  />
-                </Container>
-              </div>
-            </Collapse>
-          </div>
-        ))}
+                {thread.commentsCount === 0 ? (
+                  <Button
+                    onClick={() =>
+                      navigate("/newcomment", {
+                        state: { selectedThreadId: thread._id },
+                      })
+                    }
+                    className="btn-link no-hover nowrap-button"
+                  >
+                    New Comment
+                  </Button>
+                ) : (
+                  // If there are replies, show "View" button
+                  <Button
+                    onClick={() => toggleThread(thread._id)}
+                    aria-controls={`collapse${thread._id}`}
+                    aria-expanded={openThreads[thread._id]}
+                    className="btn-link no-hover nowrap-button"
+                  >
+                    {openThreads[thread._id] ? "Hide" : "View"}
+                  </Button>
+                )}
+              </Container>
+              <Collapse in={openThreads[thread._id]}>
+                <div id={`collapse${thread._id}`}>
+                  <Container className="comments-container">
+                    <AsyncComments
+                      userId={thread.user._id}
+                      threadId={thread._id}
+                    />
+                  </Container>
+                </div>
+              </Collapse>
+            </div>
+          ))}
+        </div>
       </Container>
     </div>
   );
@@ -178,19 +181,21 @@ const AsyncComments = ({ userId, threadId }) => {
           New Comment
         </Button>
       </div>
-      {comments.map((comment, commentIndex) => (
-        <div className="comment-item" key={commentIndex}>
-          <div className="comment-content">
-            <Emoji unified={comment.content[0]} size="50" />
+      <div className="commentScroll">
+        {comments.map((comment, commentIndex) => (
+          <div className="comment-item" key={commentIndex}>
+            <div className="comment-content">
+              <Emoji unified={comment.content[0]} size="50" />
+            </div>
+            <div className="comment-username">
+              User:<p className="title-content">{comment.user.username}</p>
+            </div>
+            <div className="comment-date">
+              Posted On:<p className="title-content">{comment.createdAt}</p>
+            </div>
           </div>
-          <div className="comment-username">
-            User:<p className="title-content">{comment.user.username}</p>
-          </div>
-          <div className="comment-date">
-            Posted On:<p className="title-content">{comment.createdAt}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
