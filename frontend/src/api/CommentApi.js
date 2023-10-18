55b8124f777aef432397184e5ff3a62fca63e698
Vinson Martin <vm84039@yahoo.com>
2023-10-17 19:11:52 -0700

    Adjusted comment

--- frontend/src/api/CommentApi.js
+++ frontend/src/api/CommentApi.js
@@ -28,5 +28,26 @@ const CommentApi = {
         }
     },
 
-}
+    postComment: async (userId, threadId, emojiCode) => {
+
+        try {
+        const response = await fetch(`${backendURI}/${userId}/thread/${threadId}/comment/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(emojiCode)
+        })
+
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+        return data;
+    } catch (error) {
+            
+    }
+
+}}
 export default CommentApi;
--- frontend/src/components/NewComment.js
+++ frontend/src/components/NewComment.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Emoji, EmojiStyle } from "emoji-picker-react";
+import EmojiPicker, {
+  EmojiStyle,
+  SkinTones,
+  Theme,
+  Categories,
+  EmojiClickData,
+  Emoji,
+  SuggestionMode,
+  SkinTonePickerLocation,
+} from "emoji-picker-react";
+
 import "../style/NewComment.css";
 import CommentApi from "../api/CommentApi";
 import Container from "react-bootstrap/esm/Container";
@@ -7,44 +17,61 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import Alert from "react-bootstrap/Alert";
-import EmojiPicker from "emoji-picker-react";
 import { useNavigate } from "react-router-dom";
 
 const NewComment = () => {
   const navigate = useNavigate();
+  const [selectedEmoji, setSelectedEmoji] = useState("1f60a");
+  const [inputValue, setInputValue] = useState("");
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const commentInfo = {
-      content: e.target.content.value,
-    };
+      content: e.target.content.value
+       };
+       const response = CommentApi.postComment(userId, threadId, selectedEmoji);
 
-    // Call the api and the login method here
-    const response = await CommentApi.SaveComment(commentInfo);
-    if (response.ok) {
-      const data = await response.json();
-      navigate("/");
+    try {
+      // Call the API and handle the response
+      const response = await CommentApi.SaveComment(commentInfo);
+      if (response.ok) {
+        const data = await response.json();
+        navigate("/");
+      } else {
+        console.error("Error saving comment:", response.status);
+      }
+    } catch (error) {
+      console.error("Error saving comment:", error);
     }
 
     // Clear input fields from this point on
     e.target.content.value = "";
   };
 
+  const onClick = (emojiData, event) => {
+    setSelectedEmoji(emojiData.emoji); // Set the selected emoji
+  };
+
   return (
     <Container className="d-flex justify-content-center align-items-center">
       <Container className="d-flex justify-content-center">
-        <EmojiPicker />
+      <EmojiPicker onEmojiClick={onClick} autoFocusSearch={false} emojiStyle={EmojiStyle.NATIVE} />
+
       </Container>
       <Container>
         <Form className="p-5" onSubmit={handleSubmit}>
           <h1>Create A New Comment</h1>
           <Form.Group className="mb-3">
             <InputGroup>
-              <Form.FloatingLabel label="Description">
+            <Form.FloatingLabel label={<Emoji unified={selectedEmoji}  />} className="mb-3">
                 <Form.Control
                   type="text"
                   placeholder="Description"
                   name="description"
+                  value={selectedEmoji} // Set the value of the form control to the selected emoji
+                  readOnly // Make the input read-only
                   required
                 />
               </Form.FloatingLabel>
